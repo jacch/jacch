6f8e611e3f8cb7cde289c07d8341e3cc0e405043
陳文傑 <jacch.tw@gmail.com>
2018-09-24 16:56:22 +0800

    找到附近有什麼礦石

--- find_mc_diamons.py
+++ find_mc_diamons.py
@@ -12,15 +12,15 @@ class mic:
     u=1
     def playerid(self,n):
         self.u=mc.getPlayerEntityId(n)
-        print "Account ID:"
-        print self.u
+        #print "Account ID:"
+        #print self.u
 
     def playerpostion(self):
         x,y,z=mc.entity.getPos(self.u)
         self.x=x
         self.y=y
         self.z=z
-        print x,y,z
+        #print x,y,z
 
     def getblockinfo(self,x,y,z):
         p=mc.getBlockWithData(x,y,z)
@@ -32,15 +32,36 @@ class mic:
         for xx in range(0,10+1):
             for zz in range(0,10+1):
               for yy in range(0,5):
-              
-                block=m.getblockinfo(self.x+xx-5,self.y+yy,self.z+zz-5)
+                block=mc.getblockinfo(self.x+xx-5,self.y+yy,self.z+zz-5)
                 if block.id==item:
                     result =1
-      if result==1:
-        return true
-      else
-        return false
-        
+        if result==1:
+            return True
+        else:
+            return False
+
+    def find_item1(self,item):
+        result=0
+        strs=""
+        block=mc.getBlocks(self.x+5,self.y+5,self.z+5,self.x-5,self.y-5,self.z-5)
+        block=set(block)
+        for y in block:
+            if item.get(y)!= None:
+                strs=strs+" "+item.get(y)
+                result =1
+
+            #print y
+            
+
+
+        if result==1:
+            return strs
+        else:
+            return "None"
+
+
+
+
 
 accountid='jacch'
 blockid=56
@@ -48,6 +69,11 @@ blockid=56
 m=mic()
 m.playerid(accountid) 
 m.playerpostion()
-find_result=m.find_item(blockid)
 
+ores={14:"黃金",15:"鐵礦",16:"煤碳",56:"鑽石"}
+find_result=m.find_item1(ores)
+if find_result=="None":
+    print "什麼都沒有"
+else:
+    print "發現了:"+find_result
 
